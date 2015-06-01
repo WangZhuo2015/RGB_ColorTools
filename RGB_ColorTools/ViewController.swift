3b364e1d8ecb7a4999c21e720f18944b91ac256e
王卓 <280034816@qq.com>
2015-06-01 12:15:19 +0900

    Initial Commit

---
+++ RGB_ColorTools/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RGB_ColorTools
+//
+//  Created by 王卓 on 15/6/1.
+//  Copyright (c) 2015年 王卓. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
